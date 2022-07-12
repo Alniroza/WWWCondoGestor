b7f1fde4f61c892a6f03822018bc9a0c4176f157
grimlin31 <nico.bernal.al@gmail.com>
2022-07-12 13:03:38 -0400

    -add: remote Mongo DB

--- database.js
+++ database.js
@@ -1,4 +1,12 @@
 import mongoose from 'mongoose';
 
-const mongoUrl = 'mongodb://localhost:27017/wwwcondo'
-export const mongoConnection = mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+const mongoUrl = `mongodb+srv://admin:admin@realmcluster.hn9gg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+// const mongoUrl = 'mongodb://localhost:27017/wwwcondo'
+
+export const mongoConnection = mongoose.connect(
+    mongoUrl,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
